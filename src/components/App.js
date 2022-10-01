86aab432511775f741a9b2a7b3507ce5d491c544
Rishabh Raikwar <108589747+rishabhraikwar98@users.noreply.github.com>
2022-10-01 11:22:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+import { useState } from "react";
+import "../styles/App.css";
 
-  
+export default function App() {
+  const [count, setCount] = useState(1);
+
+  const logic = () => {
+    if (count % 15 === 0) {
+      return "fizzbuzz";
+    } else if (count % 5 === 0) {
+      return "buzz";
+    } else if (count % 3 === 0) {
+      return "fizz";
+    }
+    else {
+      return "normal";
+    }
+  };
   return (
-    <div id="main">
-
+    <div className="App">
+      <button
+        id="increment"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+      <h1 className={
+        logic()
+      } >{count}</h1>
+      <button
+        id="decrement"
+        onClick={() => {
+          if (count > 1) setCount(count - 1);
+        }}
+      >
+        Decrement
+      </button>
     </div>
-  )
+  );
 }
-
-
-export default App;
