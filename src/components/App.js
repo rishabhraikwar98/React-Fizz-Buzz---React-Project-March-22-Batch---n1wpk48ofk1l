e52f376a67b6ced99e1f331cbeba9cc78a42a156
Rishabh Raikwar <108589747+rishabhraikwar98@users.noreply.github.com>
2022-10-03 14:20:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,14 @@ export default function App() {
   const [count, setCount] = useState(1);
 
   const logic = () => {
-    if (count % 3 === 0) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      return "fizzbuzz";
+    }
+    else if (count % 3 === 0) {
       return "fizz";
     }
-    else if (count % 5 === 0) {
-      return "buzz";
-    }
-    else if(count % 15) {
-      return "fizzbuzz"
+    else if(count % 5) {
+      return "buzz"
     }
     else {
       return "normal";
