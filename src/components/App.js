58bafc4bdbd832fe872d1383c0e3f2aa3d87f315
Rishabh Raikwar <108589747+rishabhraikwar98@users.noreply.github.com>
2022-10-11 14:08:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,29 +3,19 @@ import "../styles/App.css";
 
 export default function App() {
   const [count, setCount] = useState(1);
+  const Inc = ()=>{
+    setCount(count+1)
+  }
+  const Dec = ()=>{
+    setCount(count-1)
+  }
 
  
   return (
-    <div className="App">
-      <button
-        id="increment"
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Increment
-      </button>
-      <div id="counter" className = {count % 5 === 0 && count % 3 === 0 ? 'fizzbuzz' : (count % 3 === 0) ? 'fizz' : (count % 5 === 0 ) ? 'buzz' : 'normal'}>{count}</div>
-      <button
-        id="decrement"
-        onClick={() => {
-          if(count!==1){
-          setCount(count - 1);
-          }
-        }}
-      >
-        Decrement
-      </button>
-    </div>
+    <>
+    <button id="increment" onClick ={Inc}>Inecrement</button>
+    <div id= "counter"className= {count % 5 === 0 && count % 3 === 0 ? 'fizzbuzz' : (count % 3 === 0) ? 'fizz' : (count % 5 === 0 ) ? 'buzz' : 'normal'}>{count}</div>
+    <button id="decrement" onClick ={Dec}>Decrement</button>
+    </>
   );
 }
